cceccf476a785eba8bdb3c38ad51081116db8a28
Nesma016 <s153380@ap.be>
2026-05-31 16:09:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server draait op poort 3000");
 });
